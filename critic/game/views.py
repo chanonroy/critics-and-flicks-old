5a9bbbd0d5c374e9a00da9ebffe584b6a9306cac
Chanon Roy <chanonroy@gmail.com>
2016-03-22 22:29:04 -0400

    fixed some sorting

--- critic/game/views.py
+++ critic/game/views.py
@@ -1,15 +1,26 @@
-# from django.shortcuts import render_to_response
-# from django.template import RequestContext
+import random
 from django.shortcuts import render
 from .models import Movie, Review, Similar
 
+# film = "Gods of Egypt" (hard coded way to test)
+
 
 def index(request):
-    film = "Gods of Egypt"
-    m = Movie.objects.get(title=film)
-    r = m.review_set.all()
+    # determine count of all movies
+    m_count = Movie.objects.count()
+    # hard_code_length = 500
+    # find random integer for indexing
+    movies_int = random.randint(1, m_count)     # could hard code length to prevent db strain
+    # index movie by random primary key
+    m = Movie.objects.get(id=movies_int)
+
+    # 8 reviews in a pack, return 3
+    # r = random.sample(range(1,9), 3)
+    rev = m.review_set.all()[:3]
+    s = m.similar_set.all()
     context = {
         "movie": m,
-        "reviews": r
+        "reviews": rev,
+        "similar": s
     }
     return render(request, "index.html", context)
