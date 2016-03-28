980dbcd288c529e90fab4912d0719c34c34edd74
Chanon Roy <chanonroy@gmail.com>
2016-03-27 20:27:41 -0400

    web scrape/api tool fixes

--- critic/game/extra-test.py
+++ critic/game/extra-test.py
@@ -1,22 +1,29 @@
-from critic.secrets import *
-import requests
-import json
-
-mn_orig = str(input("Please enter a movie name\n>>> "))
-
-omdb_name = mn_orig.replace(" ", "+").lower()
-
-r = requests.get("https://www.tastekid.com/api/similar?q={}&k={}&type=movies&limit=5&info=1".format(omdb_name, tastekid_api))
-data = json.loads(r.text)
-
-dict_parse = data['Similar']['Results']
-similar = [li['Name'] for li in dict_parse]
-url = [li['yUrl'] for li in dict_parse]
-
-# youtube = data['Similar']['Info']
-# real_name = youtube[0]['Name']
-# real_url = youtube[0]['yUrl']
+# from critic.secrets import *
+# import requests
+# import json
 #
-# print(real_name, real_url)
+# mn_orig = str(input("Please enter a movie name\n>>> "))
+#
+# omdb_name = mn_orig.replace(" ", "+").lower()
+#
+# r = requests.get("https://www.tastekid.com/api/similar?q={}&k={}&type=movies&limit=5&info=1".format(omdb_name, tastekid_api))
+# data = json.loads(r.text)
+#
+# dict_parse = data['Similar']['Results']
+# similar = [li['Name'] for li in dict_parse]
+# url = [li['yUrl'] for li in dict_parse]
+#
+# # youtube = data['Similar']['Info']
+# # real_name = youtube[0]['Name']
+# # real_url = youtube[0]['yUrl']
+# #
+# # print(real_name, real_url)
+#
+# print(similar, url)
 
-print(similar, url)
+rev = ['-', '-', '-', 'This is review number 1', 'This is review number 2', 'This is review number 3', 'This is review number 4', 'This is review number 5']
+
+
+reviews = [x for x in rev if len(x) >= 5][:5]
+
+print(reviews)
--- critic/game/utils.py
+++ critic/game/utils.py
@@ -10,16 +10,14 @@ from bs4 import BeautifulSoup
 from critic.secrets import *
 from .models import Movie
 
-# take movie name as input and return the relevant URL titles
-mn_orig = str(input("Please enter a movie name\n>>> "))             # Take in movie name return as string
-rt_name = mn_orig.replace(" ", "_").lower()                         # Rotten Tomatoes has '_' in between words
-omdb_name = mn_orig.replace(" ", "+").lower()                       # OMDB has '+' in between words
-
-# TODO: Filter out 'the', 'a', 'an' from search
-
 
 def get_details():
     """ Open Movie Database API to return specific information for filtering """
+
+    mn_orig = str(input("Please enter a movie name\n>>> "))  # Take in movie name return as string
+    rt_name = mn_orig.lower().replace(" ", "_")
+    omdb_name = mn_orig.replace(" ", "+").lower()
+
     raw = requests.get("http://www.omdbapi.com/?t={}&r=json".format(omdb_name))
     # Keys: "Title", "Year", "Released", "Genre", "Director", "Actors", "Plot", "Poster" (300px image)
 
@@ -44,26 +42,27 @@ def get_details():
         print("\nProcess terminated")
         return
 
-    return director_last_name, year_filmed, actor_fn, actor_ln
+    return director_last_name, year_filmed, actor_fn, actor_ln, mn_orig, rt_name, omdb_name
 
 
 def get_reviews():
     """Grabs text for top reviews, filters them, and returns them in a list"""
-    # use requests to scrape html information on movie page
-    raw = requests.get("http://www.rottentomatoes.com/m/{}/reviews/?type=top_critics".format(rt_name))
-    soup = BeautifulSoup(raw.content, 'html.parser')
-    rev = soup.find_all("div", {"class": "the_review"})        # TODO: Error/exception if review is NULL
-    crit = soup.find_all("a", {"class": "unstyled bold articleLink"})
-
-    # extract text from beautiful soup object
-    reviews = [x.text for x in rev[0:5]]                       # TODO: consider dictionary format for key:value pair
-    critics = [x.text for x in crit[0:5]]
 
     # call above function to get movie information
     d = get_details()
 
+    # use requests to scrape html information on movie page
+    raw = requests.get("http://www.rottentomatoes.com/m/{}/reviews/?type=top_critics".format(d[5]))
+    soup = BeautifulSoup(raw.content, 'html.parser')
+    rev = soup.find_all("div", {"class": "the_review"})        # TODO: Error/exception if review is NULL
+    crit = soup.find_all("a", {"class": "unstyled bold articleLink"})
+
+    # extract text from beautiful soup object if it has something in it
+    reviews = [x.text for x in rev[:10] if len(x.text) >= 5][:5]
+    critics = [x.text for x in crit[:5]]
+
     # filter out words that will give movie away easily
-    reviews_f = [word.replace(mn_orig, "[Movie]")              # TODO: consider list comprehension or regex
+    reviews_f = [word.replace(d[4], "[Movie]")              # TODO: consider list comprehension or regex
                      .replace(d[0], "[Director]")
                      .replace(d[1], "[Date]")
                      .replace(d[2], "[Actor First Name]")
@@ -77,7 +76,7 @@ def get_reviews():
     confirm = input(str("\nIs this okay? (Y/N)"))
 
     # point to primary key object to properly construct review object
-    mov = Movie.objects.get(title=mn_orig)
+    mov = Movie.objects.get(title=d[4])
 
     # loop to return 5 reviews and save into database
     if confirm == 'y':
@@ -87,15 +86,15 @@ def get_reviews():
         print("Process terminated")
         return
 
-    return mov
+    return mov, d[4], d[6]
 
 
 def get_similar():
     """ Use 'Taste Kid' API to return similar movies - usage limit 150 per hour """
 
-    d = get_reviews()
+    get = get_reviews()
 
-    r = requests.get("https://www.tastekid.com/api/similar?q={}&k={}&type=movies&info=1&limit=5".format(omdb_name, tastekid_api))
+    r = requests.get("https://www.tastekid.com/api/similar?q={}&k={}&type=movies&info=1&limit=5".format(get[2], tastekid_api))
     data = json.loads(r.text)
 
     # parsing through nested dictionary object
@@ -107,11 +106,11 @@ def get_similar():
     real_name = youtube[0]['Name']
     real_url = youtube[0]['yUrl']
 
-    print("Similar: {}".format(similar))
+    print("\nSimilar: {}".format(similar))
     confirm = input(str("\nIs this okay? (Y/N)"))
 
     # point to primary key object to properly construct similar object
-    mov = Movie.objects.get(title=mn_orig)
+    mov = Movie.objects.get(title=get[1])
 
     # loop to set movie trailer urls and similar movie names to the database
     if confirm == 'y':
@@ -129,6 +128,6 @@ def get_similar():
                                      url5=url[4])
     else:
         print("Process terminated")
-        return
+    get_similar()
 
-get_similar()               # TODO: recursive, run function again
+get_similar()
